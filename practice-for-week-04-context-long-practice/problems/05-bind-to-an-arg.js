6638cce228dabbdc75d89f7cc9c18398a49486c1
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2022-11-16 15:31:27 -0800

    update

--- practice-for-week-04-context-long-practice/problems/05-bind-to-an-arg.js
+++ practice-for-week-04-context-long-practice/problems/05-bind-to-an-arg.js
@@ -1,6 +1,19 @@
 function bindToAnArg(func, arg) {
-  // Your code here
+  // Your
+  return  function (){
+    return func(org)
+  }
+}
+function iSpy(thing) {
+  return "I spy a " + thing;
 }
 
+let spyTree = bindToAnArg(iSpy, "tree");
+console.log(spyTree());        // => I spy a tree
+console.log(spyTree("car"));   // => I spy a tree
+
+let spyCar = bindToAnArg(iSpy, "car");
+console.log(spyCar());         // => I spy a car
+console.log(spyCar("potato")); // => I spy a car
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = bindToAnArg;
--- practice-for-week-04-context-long-practice/problems/11-bind-set-time-out.js
+++ practice-for-week-04-context-long-practice/problems/11-bind-set-time-out.js
@@ -1,7 +1,29 @@
 function boundFuncTimer(obj, func, delay) {
   // Your code here
+  let fun = func.bind(obj)
+  setTimeout(fun, delay)
+
 }
 
+
+
+
+class Animal {
+  constructor(age) {
+    this.age = age;
+  }
+
+  growOlder() {
+    this.age++;
+    console.log(this.age);
+  }
+}
+
+const dog = new Animal(1);
+const cat = new Animal(5);
+
+boundFuncTimer(dog, dog.growOlder, 500); // in 5 seconds prints: 2
+boundFuncTimer(cat, dog.growOlder, 700); // in 7 seconds prints: 6
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
